4a0278228a2c622f1e6b314b0bac95c383f62b31
Tyler Cook <tylercurtiscook@gmail.com>
2016-02-08 17:44:55 -0600

    Change layout and add some general options

--- src/com/subterranean_security/crimson/viewer/ui/screen/settings/LocalGeneral.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/settings/LocalGeneral.java
@@ -17,27 +17,84 @@
  *****************************************************************************/
 package com.subterranean_security.crimson.viewer.ui.screen.settings;
 
-import javax.swing.JPanel;
-import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.BorderLayout;
-import javax.swing.JComboBox;
+import java.awt.GridLayout;
 
 public class LocalGeneral extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	public JComboBox language;
+	public JCheckBox neverShowLicense;
+	public JCheckBox neverShowHelp;
+	public JCheckBox runInTray;
+	private JLabel label;
 
 	public LocalGeneral() {
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+		setLayout(new BorderLayout(0, 0));
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Language", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "General", TitledBorder.LEADING,
+				TitledBorder.TOP, null, new Color(51, 51, 51)));
 		add(panel);
-		panel.setLayout(new BorderLayout(0, 0));
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 440, 0 };
+		gbl_panel.rowHeights = new int[] { 30, 56, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		panel.add(panel_1, gbc_panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+
+		JLabel lblLanguage = new JLabel("Language:");
+		lblLanguage.setEnabled(false);
+		panel_1.add(lblLanguage);
+		panel_1.add(Box.createHorizontalGlue());
+
+		language = new JComboBox();
+		language.setModel(new DefaultComboBoxModel(new String[] { "English" }));
+		language.setEnabled(false);
+		panel_1.add(language);
+
+		JPanel panel_2 = new JPanel();
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.gridx = 0;
+		gbc_panel_2.gridy = 1;
+		panel.add(panel_2, gbc_panel_2);
+		panel_2.setLayout(new GridLayout(0, 2, 0, 0));
+
+		neverShowLicense = new JCheckBox("Never show startup license");
+		panel_2.add(neverShowLicense);
+
+		neverShowHelp = new JCheckBox("Never show help menus");
+		panel_2.add(neverShowHelp);
 		
-		JComboBox comboBox = new JComboBox();
-		panel.add(comboBox, BorderLayout.CENTER);
+		runInTray = new JCheckBox("Run in system tray");
+		panel_2.add(runInTray);
+		
+		label = new JLabel("");
+		panel_2.add(label);
 
 	}
 
