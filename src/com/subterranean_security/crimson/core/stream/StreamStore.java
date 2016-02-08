a0474321ece3f4c96f8dcf173573041f903f543f
Tyler Cook <tylercurtiscook@gmail.com>
2016-02-08 17:42:37 -0600

    Implement static storage for generic streams

---
+++ src/com/subterranean_security/crimson/core/stream/StreamStore.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2016 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.subterranean_security.crimson.core.stream;
+
+import java.util.HashMap;
+
+public enum StreamStore {
+	;
+
+	private static HashMap<Integer, Stream> streams = new HashMap<Integer, Stream>();
+
+	public static Stream getStream(int id) {
+		return streams.get(id);
+	}
+
+	public static void removeStream(int id) {
+		try {
+			streams.remove(id).stop();
+		} catch (NullPointerException e) {
+			// that stream doesnt exist
+		}
+	}
+
+	public static void addStream(int id, Stream s) {
+		streams.put(id, s);
+	}
+
+}
