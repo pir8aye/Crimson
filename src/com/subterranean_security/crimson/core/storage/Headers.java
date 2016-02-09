df656aceb13049a3b907a876934d1b02f8fd3a2c
Tyler Cook <tylercurtiscook@gmail.com>
2016-02-08 23:36:52 -0600

    Add enum for hostlist headers instead of using Strings

---
+++ src/com/subterranean_security/crimson/core/storage/Headers.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2016 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.subterranean_security.crimson.core.storage;
+
+public enum Headers {
+	COUNTRY, LANGUAGE, CRIMSON_VERSION, JAVA_VERSION, TIMEZONE, HOSTNAME, USERNAME, INTERNAL_IP, EXTERNAL_IP, OS_FAMILY, OS_ARCH, RAM_CAPACITY, USER_STATUS, MESSAGE_PING, RAM_USAGE, CPU_TEMPERATURE, CPU_NAME, CPU_USAGE, ACTIVE_WINDOW, SCREEN_PREVIEW, VIRTUALIZATION, MONITOR_COUNT;
+}
