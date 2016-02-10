6b77bb3d0b257ac3544187c4dcc72ea77034b784
Tyler Cook <tylercurtiscook@gmail.com>
2016-02-10 13:47:53 -0600

    Create blank detail modules

---
+++ src/com/subterranean_security/crimson/viewer/ui/screen/main/detail/WorldMap.java
@@ -0,0 +1,27 @@
+package com.subterranean_security.crimson.viewer.ui.screen.main.detail;
+
+import com.subterranean_security.crimson.sv.Profile;
+
+public class WorldMap extends DModule {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTarget(Profile p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateGraphics() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
