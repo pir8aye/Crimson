a095a9ce5c81fdb73aadb77c8cbf9a907ebc4fcf
Tyler Cook <tylercurtiscook@gmail.com>
2016-02-10 19:04:01 -0600

    Respond to alterations in superclass

--- src/com/subterranean_security/crimson/viewer/ui/screen/main/detail/WorldMap.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/main/detail/WorldMap.java
@@ -1,16 +1,29 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2016 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
 package com.subterranean_security.crimson.viewer.ui.screen.main.detail;
+
+import javax.swing.JPanel;
 
 import com.subterranean_security.crimson.sv.Profile;
 
-public class WorldMap extends DModule {
+public class WorldMap extends JPanel implements DModule {
 
 	private static final long serialVersionUID = 1L;
-
-	@Override
-	public int compareTo(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 	@Override
 	public void setTarget(Profile p) {
@@ -22,6 +35,18 @@ public class WorldMap extends DModule {
 	public void updateGraphics() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void setShowing(boolean showing) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
