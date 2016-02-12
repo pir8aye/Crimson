8b84ded4393ebb1b20d87095c7e96f35bf013854
Tyler Cook <tylercurtiscook@gmail.com>
2016-02-11 20:25:30 -0600

    Add more components and change layout to GridBag

--- src/com/subterranean_security/crimson/viewer/ui/screen/main/detail/Processor.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/main/detail/Processor.java
@@ -17,6 +17,9 @@
  *****************************************************************************/
 package com.subterranean_security.crimson.viewer.ui.screen.main.detail;
 
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -24,7 +27,9 @@ import java.awt.Insets;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import com.subterranean_security.crimson.core.proto.net.Stream.InfoParam;
@@ -32,61 +37,133 @@ import com.subterranean_security.crimson.core.stream.StreamStore;
 import com.subterranean_security.crimson.core.stream.info.InfoMaster;
 import com.subterranean_security.crimson.sv.Profile;
 
-public class DynamicProperty extends JPanel implements DModule {
+import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.traces.Trace2DLtd;
+
+public class Processor extends JPanel implements DModule {
 
 	private static final long serialVersionUID = 1L;
 
-	public DynamicProperty() {
+	ITrace2D trace = new Trace2DLtd(60);
+
+	public Processor() {
 		setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Processor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Processor", TitledBorder.CENTER,
+				TitledBorder.TOP, null, new Color(51, 51, 51)));
 		add(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 0, 0 };
-		gbl_panel.rowHeights = new int[] { 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.columnWeights = new double[] { 1.0 };
+		gbl_panel.rowHeights = new int[] { 57, 0, 0 };
 		gbl_panel.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
 
-		JLabel lblGraph = new JLabel("GRAPH");
+		trace.setColor(Color.RED);
+
+		Chart2D chart = new Chart2D();
+		chart.addTrace(trace);
+		chart.setFont(new Font("Dialog", Font.PLAIN, 8));
+		chart.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		chart.setPaintLabels(false);
+		chart.setAutoscrolls(true);
 		GridBagConstraints gbc_lblGraph = new GridBagConstraints();
-		gbc_lblGraph.insets = new Insets(0, 0, 5, 0);
+		gbc_lblGraph.fill = GridBagConstraints.BOTH;
 		gbc_lblGraph.gridx = 0;
 		gbc_lblGraph.gridy = 0;
-		panel.add(lblGraph, gbc_lblGraph);
+		panel.add(chart, gbc_lblGraph);
 
 		JPanel panel_2 = new JPanel();
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.anchor = GridBagConstraints.NORTH;
+		gbc_panel_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_panel_2.gridx = 0;
 		gbc_panel_2.gridy = 1;
 		panel.add(panel_2, gbc_panel_2);
 		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[] { 305, 129, 0 };
-		gbl_panel_2.rowHeights = new int[] { 0, 0, 0 };
-		gbl_panel_2.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
-		gbl_panel_2.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_2.columnWidths = new int[] { 65, 65, 0 };
+		gbl_panel_2.rowHeights = new int[] { 15, 15, 15, 15, 0 };
+		gbl_panel_2.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel_2.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel_2.setLayout(gbl_panel_2);
 
-		JLabel lblTotalCpuUsage = new JLabel("Total CPU Usage:");
+		JLabel lblTotalCpuUsage = new JLabel("Model:");
+		lblTotalCpuUsage.setFont(new Font("Dialog", Font.BOLD, 10));
 		GridBagConstraints gbc_lblTotalCpuUsage = new GridBagConstraints();
-		gbc_lblTotalCpuUsage.anchor = GridBagConstraints.WEST;
-		gbc_lblTotalCpuUsage.insets = new Insets(0, 0, 5, 0);
+		gbc_lblTotalCpuUsage.fill = GridBagConstraints.BOTH;
+		gbc_lblTotalCpuUsage.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTotalCpuUsage.gridx = 0;
 		gbc_lblTotalCpuUsage.gridy = 0;
 		panel_2.add(lblTotalCpuUsage, gbc_lblTotalCpuUsage);
 
-		JLabel label = new JLabel("0%");
-		GridBagConstraints gbc_label = new GridBagConstraints();
-		gbc_label.gridx = 1;
-		gbc_label.gridy = 0;
-		panel_2.add(label, gbc_label);
+		JLabel lblLoading = new JLabel("Loading...");
+		lblLoading.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblLoading.setHorizontalAlignment(SwingConstants.TRAILING);
+		GridBagConstraints gbc_lblLoading = new GridBagConstraints();
+		gbc_lblLoading.fill = GridBagConstraints.BOTH;
+		gbc_lblLoading.insets = new Insets(0, 0, 5, 0);
+		gbc_lblLoading.gridx = 1;
+		gbc_lblLoading.gridy = 0;
+		panel_2.add(lblLoading, gbc_lblLoading);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(null, "RAM", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		add(panel_1);
+		JLabel lblNewLabel = new JLabel("Clock Speed:");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 10));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 1;
+		panel_2.add(lblNewLabel, gbc_lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Loading...");
+		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.TRAILING);
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 1;
+		panel_2.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Logical Processors:");
+		lblNewLabel_2.setFont(new Font("Dialog", Font.BOLD, 10));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 2;
+		panel_2.add(lblNewLabel_2, gbc_lblNewLabel_2);
+
+		JLabel lblNewLabel_3 = new JLabel("Loading...");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_3.setFont(new Font("Dialog", Font.BOLD, 10));
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_3.gridx = 1;
+		gbc_lblNewLabel_3.gridy = 2;
+		panel_2.add(lblNewLabel_3, gbc_lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("Current Utilization:");
+		lblNewLabel_4.setFont(new Font("Dialog", Font.BOLD, 10));
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_4.gridx = 0;
+		gbc_lblNewLabel_4.gridy = 3;
+		panel_2.add(lblNewLabel_4, gbc_lblNewLabel_4);
+
+		JLabel lblNewLabel_5 = new JLabel("Loading...");
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_5.setFont(new Font("Dialog", Font.BOLD, 10));
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_5.gridx = 1;
+		gbc_lblNewLabel_5.gridy = 3;
+		panel_2.add(lblNewLabel_5, gbc_lblNewLabel_5);
+
 	}
 
 	private int streamID;
