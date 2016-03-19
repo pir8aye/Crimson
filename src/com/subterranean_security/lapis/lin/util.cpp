122223c8b709f7bcf0ffbfc80bef3ba891a7ea77
tylercurtiscook <tylercurtiscook@gmail.com>
2016-03-19 17:43:25 -0500

    Blank implementation

---
+++ src/com/subterranean_security/lapis/lin/util.cpp
@@ -0,0 +1,12 @@
+#include <com_subterranean_security_crimson_client_Native.h>
+
+JNIEXPORT jstring JNICALL Java_com_subterranean_1security_crimson_client_Native_getActiveWindow(
+		JNIEnv *env, jclass cls) {
+
+	return null;
+}
+
+JNIEXPORT jlong JNICALL Java_com_subterranean_1security_crimson_client_Native_getSystemUptime(
+		JNIEnv *env, jclass cls) {
+	return 0;
+}
