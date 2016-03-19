f1bd6e0cf3230a9a20b249cf23e53e37072f492a
tylercurtiscook <tylercurtiscook@gmail.com>
2016-03-19 18:13:26 -0500

    Add temporary method for finding cpu temps

--- src/com/subterranean_security/crimson/client/Native.java
+++ src/com/subterranean_security/crimson/client/Native.java
@@ -17,9 +17,53 @@
  *****************************************************************************/
 package com.subterranean_security.crimson.client;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
 public class Native {
 
 	public static native String getActiveWindow();
+
 	public static native long getSystemUptime();
+
+	// move this!
+	private static ArrayList<RandomAccessFile> cores = new ArrayList<RandomAccessFile>();
+	private static int maxCores = 64;
+
+	public static double[] getLinuxCPUTemps() {
+
+		if (cores.size() == 0) {
+			for (int i = 2; i < maxCores + 2; i++) {
+				File f = new File("/sys/class/hwmon/hwmon1/temp" + i + "_input");
+				if (f.exists()) {
+					try {
+						cores.add(new RandomAccessFile(f, "r"));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					break;
+				}
+			}
+		}
+		double[] temps = new double[cores.size()];
+		for (int i = 0; i < cores.size(); i++) {
+			try {
+				cores.get(i).seek(0);
+				temps[i] = cores.get(i).readDouble();
+				System.out.println("Core " + i + " temp: " + temps[i]);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return null;
+	}
 
 }
