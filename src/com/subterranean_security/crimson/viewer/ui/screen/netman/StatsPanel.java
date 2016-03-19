3759741668c23c7343534ea40785468997cd77d4
tylercurtiscook <tylercurtiscook@gmail.com>
2016-03-19 18:16:32 -0500

    Restructure slightly

--- src/com/subterranean_security/crimson/viewer/ui/screen/netman/StatsPanel.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/netman/StatsPanel.java
@@ -5,35 +5,47 @@ import java.awt.GridLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
 
 public class StatsPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
 	public StatsPanel() {
-		setLayout(new BorderLayout(0, 0));
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
 		JPanel panel = new JPanel();
-		add(panel, BorderLayout.CENTER);
-		panel.setLayout(new GridLayout(0, 2, 0, 0));
-
-		JLabel lblListeners = new JLabel("Listeners:");
-		panel.add(lblListeners);
-
-		JLabel label = new JLabel("0");
-		panel.add(label);
-
-		JLabel lblInternalIp = new JLabel("Internal IP:");
-		panel.add(lblInternalIp);
-
-		JLabel label_1 = new JLabel("0.0.0.0");
-		panel.add(label_1);
-
-		JLabel lblExternalIp = new JLabel("External IP:");
-		panel.add(lblExternalIp);
-
-		JLabel label_2 = new JLabel("0.0.0.0");
-		panel.add(label_2);
+		add(panel);
+				panel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+				JLabel lblListeners = new JLabel("Listeners:");
+				panel.add(lblListeners);
+						
+								JLabel label = new JLabel("0");
+								label.setHorizontalAlignment(SwingConstants.TRAILING);
+								panel.add(label);
+				
+						JLabel lblInternalIp = new JLabel("Internal IP:");
+						panel.add(lblInternalIp);
+				
+						JLabel label_1 = new JLabel("0.0.0.0");
+						label_1.setHorizontalAlignment(SwingConstants.TRAILING);
+						panel.add(label_1);
+		
+				JLabel lblExternalIp = new JLabel("External IP:");
+				panel.add(lblExternalIp);
+		
+				JLabel label_2 = new JLabel("0.0.0.0");
+				label_2.setHorizontalAlignment(SwingConstants.TRAILING);
+				panel.add(label_2);
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1);
+		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
 
 	}
 
