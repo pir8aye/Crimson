eec379d29ca42ac3debd0ad9094e06d5e1c7eea5
Tyler Cook <tylercurtiscook@gmail.com>
2016-05-22 21:23:42 -0500

    Wrap in HPanel

--- src/com/subterranean_security/crimson/viewer/ui/screen/netman/NetMan.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/netman/NetMan.java
@@ -1,5 +1,6 @@
 package com.subterranean_security.crimson.viewer.ui.screen.netman;
 
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
@@ -9,13 +10,15 @@ import javax.swing.JTabbedPane;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 
+import com.subterranean_security.crimson.viewer.ui.panel.HPanel;
 import com.subterranean_security.crimson.viewer.ui.utility.UIStore;
 import com.subterranean_security.crimson.viewer.ui.utility.UUtil;
 
 public class NetMan extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel contentPane = new JPanel();
+	private HPanel hp = new HPanel(contentPane);
 	public ListenerPanel lp;
 
 	public NetMan() {
@@ -23,9 +26,8 @@ public class NetMan extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setMinimumSize(new Dimension(573, 364));
 		setTitle("Network Manager");
-		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		setContentPane(hp);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
 		StatsPanel sp = new StatsPanel();
@@ -42,6 +44,13 @@ public class NetMan extends JFrame {
 		AuthPanel ap = new AuthPanel();
 		tabbedPane.add(ap);
 		tabbedPane.setTitleAt(1, "Authentication");
+
+		Component[] buttons = { hp.initBtnUP() };
+		hp.nmenu.setButtons(buttons);
+
+		hp.hmenu.setDesc("Manages listeners and authentication on the server.  At least one listener must be defined.");
+
+		hp.refreshHeight();
 	}
 
 	@Override
--- src/com/subterranean_security/crimson/viewer/ui/screen/netman/StatsPanel.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/netman/StatsPanel.java
@@ -1,8 +1,10 @@
 package com.subterranean_security.crimson.viewer.ui.screen.netman;
 
-import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -12,36 +14,99 @@ public class StatsPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public StatsPanel() {
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 146, 104, 18, 77, 51, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
 
-		JPanel panel = new JPanel();
-		add(panel);
-				panel.setLayout(new GridLayout(0, 2, 0, 0));
-		
-				JLabel lblListeners = new JLabel("Listeners:");
-				panel.add(lblListeners);
-						
-								JLabel label = new JLabel("0");
-								label.setHorizontalAlignment(SwingConstants.TRAILING);
-								panel.add(label);
-				
-						JLabel lblInternalIp = new JLabel("Internal IP:");
-						panel.add(lblInternalIp);
-				
-						JLabel label_1 = new JLabel("0.0.0.0");
-						label_1.setHorizontalAlignment(SwingConstants.TRAILING);
-						panel.add(label_1);
-		
-				JLabel lblExternalIp = new JLabel("External IP:");
-				panel.add(lblExternalIp);
-		
-				JLabel label_2 = new JLabel("0.0.0.0");
-				label_2.setHorizontalAlignment(SwingConstants.TRAILING);
-				panel.add(label_2);
-		
-		JPanel panel_1 = new JPanel();
-		add(panel_1);
-		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
+		JLabel label = new JLabel("Listeners:");
+		label.setFont(new Font("Dialog", Font.BOLD, 11));
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.WEST;
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 0;
+		add(label, gbc_label);
+
+		JLabel label_1 = new JLabel("0");
+		label_1.setHorizontalAlignment(SwingConstants.TRAILING);
+		label_1.setFont(new Font("Dialog", Font.PLAIN, 11));
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.anchor = GridBagConstraints.EAST;
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.gridx = 1;
+		gbc_label_1.gridy = 0;
+		add(label_1, gbc_label_1);
+
+		JLabel lblClients = new JLabel("Clients:");
+		lblClients.setFont(new Font("Dialog", Font.BOLD, 11));
+		GridBagConstraints gbc_lblClients = new GridBagConstraints();
+		gbc_lblClients.insets = new Insets(0, 0, 5, 5);
+		gbc_lblClients.gridx = 3;
+		gbc_lblClients.gridy = 0;
+		add(lblClients, gbc_lblClients);
+
+		JLabel lblNewLabel = new JLabel("val");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 4;
+		gbc_lblNewLabel.gridy = 0;
+		add(lblNewLabel, gbc_lblNewLabel);
+
+		JLabel lblLocalInternalIp = new JLabel("Local Internal IP:");
+		lblLocalInternalIp.setFont(new Font("Dialog", Font.BOLD, 11));
+		GridBagConstraints gbc_lblLocalInternalIp = new GridBagConstraints();
+		gbc_lblLocalInternalIp.anchor = GridBagConstraints.WEST;
+		gbc_lblLocalInternalIp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLocalInternalIp.gridx = 0;
+		gbc_lblLocalInternalIp.gridy = 1;
+		add(lblLocalInternalIp, gbc_lblLocalInternalIp);
+
+		JLabel label_3 = new JLabel("0.0.0.0");
+		label_3.setHorizontalAlignment(SwingConstants.TRAILING);
+		label_3.setFont(new Font("Dialog", Font.PLAIN, 11));
+		GridBagConstraints gbc_label_3 = new GridBagConstraints();
+		gbc_label_3.anchor = GridBagConstraints.EAST;
+		gbc_label_3.insets = new Insets(0, 0, 5, 5);
+		gbc_label_3.gridx = 1;
+		gbc_label_3.gridy = 1;
+		add(label_3, gbc_label_3);
+
+		JLabel lblViewers = new JLabel("Viewers:");
+		lblViewers.setFont(new Font("Dialog", Font.BOLD, 11));
+		GridBagConstraints gbc_lblViewers = new GridBagConstraints();
+		gbc_lblViewers.insets = new Insets(0, 0, 5, 5);
+		gbc_lblViewers.gridx = 3;
+		gbc_lblViewers.gridy = 1;
+		add(lblViewers, gbc_lblViewers);
+
+		JLabel lblVal = new JLabel("val");
+		GridBagConstraints gbc_lblVal = new GridBagConstraints();
+		gbc_lblVal.insets = new Insets(0, 0, 5, 0);
+		gbc_lblVal.gridx = 4;
+		gbc_lblVal.gridy = 1;
+		add(lblVal, gbc_lblVal);
+
+		JLabel lblServerExternalIp = new JLabel("Server External IP:");
+		lblServerExternalIp.setFont(new Font("Dialog", Font.BOLD, 11));
+		GridBagConstraints gbc_lblServerExternalIp = new GridBagConstraints();
+		gbc_lblServerExternalIp.anchor = GridBagConstraints.WEST;
+		gbc_lblServerExternalIp.insets = new Insets(0, 0, 0, 5);
+		gbc_lblServerExternalIp.gridx = 0;
+		gbc_lblServerExternalIp.gridy = 2;
+		add(lblServerExternalIp, gbc_lblServerExternalIp);
+
+		JLabel label_5 = new JLabel("0.0.0.0");
+		label_5.setHorizontalAlignment(SwingConstants.TRAILING);
+		label_5.setFont(new Font("Dialog", Font.PLAIN, 11));
+		GridBagConstraints gbc_label_5 = new GridBagConstraints();
+		gbc_label_5.anchor = GridBagConstraints.EAST;
+		gbc_label_5.insets = new Insets(0, 0, 0, 5);
+		gbc_label_5.gridx = 1;
+		gbc_label_5.gridy = 2;
+		add(label_5, gbc_label_5);
 
 	}
 
