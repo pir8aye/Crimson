101a7b24fb4fbcdb3c5f286d5f048e506efadded
Tyler Cook <tylercurtiscook@gmail.com>
2016-06-30 12:36:58 -0500

    Improve statpanel visuals

--- src/com/subterranean_security/crimson/viewer/ui/screen/netman/StatsPanel.java
+++ src/com/subterranean_security/crimson/viewer/ui/screen/netman/StatsPanel.java
@@ -11,6 +11,9 @@ import javax.swing.SwingConstants;
 
 import com.subterranean_security.crimson.core.util.CUtil;
 import com.subterranean_security.crimson.viewer.ViewerStore;
+import com.subterranean_security.crimson.viewer.ui.UIUtil;
+import javax.swing.border.EtchedBorder;
+import java.awt.Dimension;
 
 public class StatsPanel extends JPanel {
 
@@ -27,105 +30,78 @@ public class StatsPanel extends JPanel {
 	}
 
 	public void init() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 146, 104, 48, 77, 51, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
-		setLayout(gridBagLayout);
+
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(250, 60));
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		add(panel);
+		panel.setLayout(null);
 
 		JLabel label = new JLabel("Listeners:");
-		label.setFont(new Font("Dialog", Font.BOLD, 11));
-		GridBagConstraints gbc_label = new GridBagConstraints();
-		gbc_label.anchor = GridBagConstraints.WEST;
-		gbc_label.insets = new Insets(0, 0, 5, 5);
-		gbc_label.gridx = 0;
-		gbc_label.gridy = 0;
-		add(label, gbc_label);
+		label.setIcon(UIUtil.getIcon("icons16/general/network_ethernet.png"));
+		label.setBounds(6, 6, 125, 16);
+		label.setFont(new Font("Dialog", Font.BOLD, 10));
+		panel.add(label);
 
 		label_1 = new JLabel("loading...");
+		label_1.setBounds(143, 6, 98, 16);
 		label_1.setHorizontalAlignment(SwingConstants.TRAILING);
-		label_1.setFont(new Font("Dialog", Font.PLAIN, 11));
-		GridBagConstraints gbc_label_1 = new GridBagConstraints();
-		gbc_label_1.anchor = GridBagConstraints.EAST;
-		gbc_label_1.insets = new Insets(0, 0, 5, 5);
-		gbc_label_1.gridx = 1;
-		gbc_label_1.gridy = 0;
-		add(label_1, gbc_label_1);
-
-		JLabel lblClients = new JLabel("Clients:");
-		lblClients.setFont(new Font("Dialog", Font.BOLD, 11));
-		GridBagConstraints gbc_lblClients = new GridBagConstraints();
-		gbc_lblClients.insets = new Insets(0, 0, 5, 5);
-		gbc_lblClients.gridx = 3;
-		gbc_lblClients.gridy = 0;
-		add(lblClients, gbc_lblClients);
-
-		lblNewLabel = new JLabel("loading...");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 11));
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel.gridx = 4;
-		gbc_lblNewLabel.gridy = 0;
-		add(lblNewLabel, gbc_lblNewLabel);
+		label_1.setFont(new Font("Dialog", Font.PLAIN, 10));
+		panel.add(label_1);
 
 		JLabel lblLocalInternalIp = new JLabel("Local Internal IP:");
-		lblLocalInternalIp.setFont(new Font("Dialog", Font.BOLD, 11));
-		GridBagConstraints gbc_lblLocalInternalIp = new GridBagConstraints();
-		gbc_lblLocalInternalIp.anchor = GridBagConstraints.WEST;
-		gbc_lblLocalInternalIp.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLocalInternalIp.gridx = 0;
-		gbc_lblLocalInternalIp.gridy = 1;
-		add(lblLocalInternalIp, gbc_lblLocalInternalIp);
+		lblLocalInternalIp.setBounds(6, 24, 125, 16);
+		lblLocalInternalIp.setIcon(UIUtil.getIcon("icons16/general/ip.png"));
+		lblLocalInternalIp.setFont(new Font("Dialog", Font.BOLD, 10));
+		panel.add(lblLocalInternalIp);
 
 		label_3 = new JLabel("loading...");
+		label_3.setBounds(143, 24, 98, 16);
 		label_3.setHorizontalAlignment(SwingConstants.TRAILING);
-		label_3.setFont(new Font("Dialog", Font.PLAIN, 11));
-		GridBagConstraints gbc_label_3 = new GridBagConstraints();
-		gbc_label_3.anchor = GridBagConstraints.EAST;
-		gbc_label_3.insets = new Insets(0, 0, 5, 5);
-		gbc_label_3.gridx = 1;
-		gbc_label_3.gridy = 1;
-		add(label_3, gbc_label_3);
-
-		JLabel lblViewers = new JLabel("Viewers:");
-		lblViewers.setFont(new Font("Dialog", Font.BOLD, 11));
-		GridBagConstraints gbc_lblViewers = new GridBagConstraints();
-		gbc_lblViewers.insets = new Insets(0, 0, 5, 5);
-		gbc_lblViewers.gridx = 3;
-		gbc_lblViewers.gridy = 1;
-		add(lblViewers, gbc_lblViewers);
-
-		lblVal = new JLabel("loading...");
-		lblVal.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblVal.setFont(new Font("Dialog", Font.PLAIN, 11));
-		GridBagConstraints gbc_lblVal = new GridBagConstraints();
-		gbc_lblVal.anchor = GridBagConstraints.EAST;
-		gbc_lblVal.insets = new Insets(0, 0, 5, 0);
-		gbc_lblVal.gridx = 4;
-		gbc_lblVal.gridy = 1;
-		add(lblVal, gbc_lblVal);
+		label_3.setFont(new Font("Dialog", Font.PLAIN, 10));
+		panel.add(label_3);
 
 		JLabel lblServerExternalIp = new JLabel("Server External IP:");
-		lblServerExternalIp.setFont(new Font("Dialog", Font.BOLD, 11));
-		GridBagConstraints gbc_lblServerExternalIp = new GridBagConstraints();
-		gbc_lblServerExternalIp.anchor = GridBagConstraints.WEST;
-		gbc_lblServerExternalIp.insets = new Insets(0, 0, 0, 5);
-		gbc_lblServerExternalIp.gridx = 0;
-		gbc_lblServerExternalIp.gridy = 2;
-		add(lblServerExternalIp, gbc_lblServerExternalIp);
+		lblServerExternalIp.setBounds(6, 40, 139, 16);
+		lblServerExternalIp.setIcon(UIUtil.getIcon("icons16/general/ip.png"));
+		lblServerExternalIp.setFont(new Font("Dialog", Font.BOLD, 10));
+		panel.add(lblServerExternalIp);
 
 		label_5 = new JLabel("loading...");
+		label_5.setBounds(143, 40, 98, 16);
 		label_5.setHorizontalAlignment(SwingConstants.TRAILING);
-		label_5.setFont(new Font("Dialog", Font.PLAIN, 11));
-		GridBagConstraints gbc_label_5 = new GridBagConstraints();
-		gbc_label_5.anchor = GridBagConstraints.EAST;
-		gbc_label_5.insets = new Insets(0, 0, 0, 5);
-		gbc_label_5.gridx = 1;
-		gbc_label_5.gridy = 2;
-		add(label_5, gbc_label_5);
+		label_5.setFont(new Font("Dialog", Font.PLAIN, 10));
+		panel.add(label_5);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_1.setPreferredSize(new Dimension(250, 60));
+		add(panel_1);
+		panel_1.setLayout(null);
+
+		JLabel lblClients = new JLabel("Clients:");
+		lblClients.setIcon(UIUtil.getIcon("icons16/general/user.png"));
+		lblClients.setBounds(6, 6, 75, 16);
+		panel_1.add(lblClients);
+		lblClients.setFont(new Font("Dialog", Font.BOLD, 10));
+
+		lblNewLabel = new JLabel("loading...");
+		lblNewLabel.setBounds(143, 6, 98, 16);
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 10));
+
+		JLabel lblViewers = new JLabel("Viewers:");
+		lblViewers.setBounds(6, 24, 75, 16);
+		panel_1.add(lblViewers);
+		lblViewers.setIcon(UIUtil.getIcon("icons16/general/viewer.png"));
+		lblViewers.setFont(new Font("Dialog", Font.BOLD, 10));
+
+		lblVal = new JLabel("loading...");
+		lblVal.setBounds(143, 24, 98, 16);
+		panel_1.add(lblVal);
+		lblVal.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblVal.setFont(new Font("Dialog", Font.PLAIN, 10));
 
 	}
 
