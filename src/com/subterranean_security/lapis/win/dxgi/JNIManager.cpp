ae9ef4b1a176d53a5fbd4d52183ca9db0190aa15
Tyler Cook <tylercurtiscook@gmail.com>
2016-07-23 15:14:58 -0500

    Improve RD code quality

--- src/com/subterranean_security/lapis/win/dxgi/JNIManager.cpp
+++ src/com/subterranean_security/lapis/win/dxgi/JNIManager.cpp
@@ -9,19 +9,16 @@ jmethodID callback_remote_moveRect;
 jmethodID callback_remote_dirtyRect;
 
 void initJNIManager(JNIEnv *e) {
-	std::cout << "initializing JNIManager" << std::endl;
 	e->GetJavaVM(&jvm);
 }
 
 void initEnv() {
-	std::cout << "Initializing JNIEnv" << std::endl;
 	JavaVMAttachArgs args;
 	args.version = JNI_VERSION_1_8; // choose your JNI version
 	args.name = NULL; // you might want to give the java thread a name
 	args.group = NULL; // you might want to assign the java thread to a ThreadGroup
 	jvm->AttachCurrentThread((void**) &env, &args);
 
-	std::cout << "Locating Java methods" << std::endl;
 	nativeCls = env->FindClass(
 			"com/subterranean_security/crimson/core/util/Native");
 	if (nativeCls == nullptr) {
@@ -32,8 +29,16 @@ void initEnv() {
 			"callback_remote_moveRect", "(IIIIII)V");
 	callback_remote_dirtyRect = env->GetStaticMethodID(nativeCls,
 			"callback_remote_dirtyRect", "(IIII[I)V");
-	std::cout << "Initialized JNIEnv" << std::endl;
 
+}
+
+void printDesc(D3D11_TEXTURE2D_DESC* desc) {
+	std::cout << "Width:" << desc->Width << std::endl;
+	std::cout << "Height:" << desc->Height << std::endl;
+	std::cout << "MipLevels:" << desc->MipLevels << std::endl;
+	std::cout << "ArraySize:" << desc->ArraySize << std::endl;
+	std::cout << "Format:" << desc->Format << std::endl;
+	std::cout << "MiscFlags:" << desc->MiscFlags << std::endl;
 }
 
 void sendFrameJNI(THREAD_DATA* TData, FRAME_DATA *CurrentData) {
@@ -53,23 +58,37 @@ void sendFrameJNI(THREAD_DATA* TData, FRAME_DATA *CurrentData) {
 	TData->DxRes.Device->GetImmediateContext(&context);
 
 // get description of original texture
-	D3D11_TEXTURE2D_DESC ppDesc;
-	CurrentData->Frame->GetDesc(&ppDesc);
+	D3D11_TEXTURE2D_DESC oldDesc;
+	CurrentData->Frame->GetDesc(&oldDesc);
+	std::cout << "Printing Old Description:" << std::endl;
+	printDesc(&oldDesc);
 
-//modify original description
+//create new description
+	D3D11_TEXTURE2D_DESC ppDesc;
+	ZeroMemory(&ppDesc, sizeof(ppDesc));
+	ppDesc.Width = oldDesc.Width;
+	ppDesc.Height = oldDesc.Height;
+	ppDesc.MipLevels = 1;
+	ppDesc.ArraySize = 1;
+	ppDesc.Format = oldDesc.Format;
+	ppDesc.SampleDesc.Count = 1;
+	ppDesc.SampleDesc.Quality = 0;
 	ppDesc.Usage = D3D11_USAGE_STAGING;
-	ppDesc.BindFlags = 0;
 	ppDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
+	ppDesc.MiscFlags = 0;
 
 // get ID3D11Device from thread data and create texture
 	TData->DxRes.Device->CreateTexture2D(&ppDesc, NULL, &ppTexture2D);
+
+	std::cout << "Printing new Description:" << std::endl;
+	printDesc(&ppDesc);
 
 // perform copy
 	context->CopyResource(CurrentData->Frame, ppTexture2D);
 
 // get mapped subresource
 	D3D11_MAPPED_SUBRESOURCE res;
-	HRESULT hr = context->Map(ppTexture2D, 0, D3D11_MAP_READ, 0, &res);
+	HRESULT hr = context->Map(ppTexture2D, 0, D3D11_MAP_READ, NULL, &res);
 	if (!SUCCEEDED(hr)) {
 		std::cout << "MAP FAILED :(" << std::endl;
 	}
@@ -93,7 +112,8 @@ void sendFrameJNI(THREAD_DATA* TData, FRAME_DATA *CurrentData) {
 		RECT* rect = reinterpret_cast<RECT*>(CurrentData->MetaData
 				+ (CurrentData->MoveCount * sizeof(DXGI_OUTDUPL_MOVE_RECT)));
 
-		UINT dataSize = (rect->right - rect->left) * (rect->bottom - rect->top);
+		//UINT dataSize = (rect->right - rect->left) * (rect->bottom - rect->top);
+		UINT dataSize = ppDesc.Width + ppDesc.Height;
 		jintArray data = env->NewIntArray(dataSize);
 
 		int *rand = static_cast<int*>(res.pData);
@@ -109,14 +129,22 @@ void sendFrameJNI(THREAD_DATA* TData, FRAME_DATA *CurrentData) {
 				rect->left, rect->top, rect->right - rect->left,
 				rect->bottom - rect->top, data);
 
-	}
+		//	struct Color {
+		//		float r, g, b, a;
+		//	};
+		//	Color* obj;
+		//	obj = new Color[(res.RowPitch / sizeof(Color)) * ppDesc.Height];
+		//	std::cout << "memcpy" << std::endl;
+		//	memcpy(obj, res.pData, res.RowPitch * ppDesc.Height);
+		//	std::cout << "test values" << std::endl;
+		//	int sum2 = 0;
+		//	for (int i = 0; i < (res.RowPitch / sizeof(Color)) * ppDesc.Height;
+		//			i++) {
+		//		sum2 += obj[i].r;
+		//	}
+		//	std::cout << "sum2: " << sum2 << std::endl;
 
-//	struct Color {
-//		float r, g, b, a;
-//	};
-//	Color* obj;
-//	obj = new Color[(res.RowPitch / sizeof(Color)) * ppDesc.Height];
-//	memcpy(obj, res.pData, res.RowPitch * ppDesc.Height);
+	}
 
 // unmap
 	context->Unmap(ppTexture2D, 0);
