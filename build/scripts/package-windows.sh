114c0f6b8567f3152dd0630f20ab5bc4ca58ccba
Tyler Cook <tylercurtiscook@gmail.com>
2017-06-15 16:17:09 -0500

    Deprecate and remove package scripts

--- build/scripts/package-windows.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's|[0-9].[0-9].[0-9].[0-9]\+|'$1'|g' /home/subterranean/Workspace/Crimson/build/platform/exe/l4j.xml
-/opt/launch4j/launch4j /home/subterranean/Workspace/Crimson/build/platform/exe/l4j.xml
