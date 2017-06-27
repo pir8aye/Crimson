3132281361ffbbf92a651b0776c2189d32c89dd4
Tyler Cook <tylercurtiscook@gmail.com>
2017-06-26 20:55:10 -0500

    Remove ServerConnectionStore

--- src/main/java/com/subterranean_security/crimson/server/net/listener/ServerConnectionEventListener.java
+++ src/main/java/com/subterranean_security/crimson/server/net/listener/ServerConnectionEventListener.java
@@ -19,6 +19,9 @@ package com.subterranean_security.crimson.server.net.listener;
 
 import java.util.Observable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.subterranean_security.crimson.core.attribute.keys.AttributeKey;
 import com.subterranean_security.crimson.core.attribute.keys.singular.AKeySimple;
 import com.subterranean_security.crimson.core.net.Connector;
@@ -26,15 +29,19 @@ import com.subterranean_security.crimson.core.net.Connector.ConnectionState;
 import com.subterranean_security.crimson.core.net.listener.ConnectionEventListener;
 import com.subterranean_security.crimson.core.store.NetworkStore;
 import com.subterranean_security.crimson.proto.core.net.sequences.Delta.AttributeGroupContainer;
+import com.subterranean_security.crimson.proto.core.net.sequences.Delta.EV_NetworkDelta;
+import com.subterranean_security.crimson.proto.core.net.sequences.Delta.EV_NetworkDelta.LinkAdded;
+import com.subterranean_security.crimson.proto.core.net.sequences.Delta.EV_NetworkDelta.LinkRemoved;
 import com.subterranean_security.crimson.proto.core.net.sequences.Delta.EV_ProfileDelta;
 import com.subterranean_security.crimson.proto.core.net.sequences.MSG.Message;
-import com.subterranean_security.crimson.server.net.ServerConnectionStore;
 import com.subterranean_security.crimson.server.store.AuthStore;
 import com.subterranean_security.crimson.server.store.ServerProfileStore;
 import com.subterranean_security.crimson.sv.permissions.Perm;
 import com.subterranean_security.crimson.universal.Universal.Instance;
 
 public class ServerConnectionEventListener extends ConnectionEventListener {
+
+	private static final Logger log = LoggerFactory.getLogger(ServerConnectionEventListener.class);
 
 	@Override
 	public void update(Observable o, Object arg) {
@@ -46,13 +53,13 @@ public class ServerConnectionEventListener extends ConnectionEventListener {
 				switch ((ConnectionState) arg) {
 				case AUTHENTICATED:
 					clientAuthenticated(connector);
-					ServerConnectionStore.addLink(connector.getCvid(), 0);
+					addLink(connector.getCvid(), 0);
 					break;
 				case CONNECTED:
 					break;
 				case NOT_CONNECTED:
 					clientNotConnected(connector);
-					ServerConnectionStore.removeLink(connector.getCvid(), 0);
+					removeLink(connector.getCvid(), 0);
 					break;
 				default:
 					break;
@@ -87,6 +94,22 @@ public class ServerConnectionEventListener extends ConnectionEventListener {
 										.setGroupType(AttributeKey.Type.GENERAL.ordinal()).putAttribute(
 												AKeySimple.CLIENT_ONLINE.ordinal(), "0")
 										.build())));
+	}
+
+	public static void addLink(int cvid1, int cvid2) {
+		log.debug("Linking: {} and {}", cvid1, cvid2);
+		EV_NetworkDelta.Builder ev = EV_NetworkDelta.newBuilder()
+				.setLinkAdded(LinkAdded.newBuilder().setCvid1(cvid1).setCvid2(cvid2));
+
+		NetworkStore.broadcastTo(Instance.VIEWER, Message.newBuilder().setEvNetworkDelta(ev).build());
+	}
+
+	public static void removeLink(int cvid1, int cvid2) {
+		log.debug("Delinking: {} and {}", cvid1, cvid2);
+		EV_NetworkDelta.Builder ev = EV_NetworkDelta.newBuilder()
+				.setLinkRemoved(LinkRemoved.newBuilder().setCvid1(cvid1).setCvid2(cvid2));
+
+		NetworkStore.broadcastTo(Instance.VIEWER, Message.newBuilder().setEvNetworkDelta(ev).build());
 	}
 
 }
